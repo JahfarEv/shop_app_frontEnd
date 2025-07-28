f655f55206842b4bbd228fcab0d81d5b42d36ee8
Jahfar Ali <evjahfar@gmail.com>
2025-07-28 13:08:16 +0530

    sales_registration

--- src/components/SalesmanRegistration.jsx
+++ src/components/SalesmanRegistration.jsx
@@ -28,7 +28,7 @@ const SalesmanRegistration = () => {
     const fetchManagers = async () => {
       try {
         setLoadingManagers(true);
-        const response = await fetch('http://localhost:8000/api/manager/managers');
+        const response = await fetch('https://shop-app-backend-k0a2.onrender.com/api/manager/managers');
         const data = await response.json();
         if (response.ok) {
           setManagers(data);
